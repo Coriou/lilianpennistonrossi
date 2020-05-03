fbb1dc380e175631d33a35d2895e2bcfb3558c0c
Benjamin C <benjamin@coriou.net>
2020-05-03 15:18:11 +0200

    Replace SVG logo with PNG
    10x smaller, still high res

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,15 +1,46 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
 import useWindowSize from "../useWindowSize"
-import LogoLong from "../images/lilian-logo-long-holly.svg"
-import LogoSmall from "../images/lilian-logo-small-holly.svg"
+// import LogoLong from "../images/lilian-logo-long-holly.svg"
+// import LogoSmall from "../images/lilian-logo-small-holly.svg"
 
 export default () => {
+	const {
+		allFile: { edges },
+	} = useStaticQuery(
+		graphql`
+			query {
+				allFile(
+					filter: {
+						name: { eq: "lilian-logo-long-holly" }
+						extension: { eq: "png" }
+					}
+				) {
+					edges {
+						node {
+							long: childImageSharp {
+								fixed(width: 572, height: 52, quality: 100) {
+									...GatsbyImageSharpFixed
+								}
+							}
+							small: childImageSharp {
+								fixed(width: 200, height: 52, quality: 100) {
+									...GatsbyImageSharpFixed
+								}
+							}
+						}
+					}
+				}
+			}
+		`
+	)
+
 	const { width } = useWindowSize()
 
 	if (width <= 768)
 		return (
-			<LogoSmall height="25.6px" width="100px" className="logo" alt="Logo" />
+			<img src={edges[0].node.small.fixed.src} className="logo" alt="Logo" />
 		)
 
-	return <LogoLong height="25.6px" width="286px" className="logo" alt="Logo" />
+	return <img src={edges[0].node.long.fixed.src} className="logo" alt="Logo" />
 }
