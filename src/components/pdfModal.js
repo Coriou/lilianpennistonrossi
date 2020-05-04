90f8fd89f34f2526a3355b4c3ff89537a433c7a7
Benjamin C <benjamin@coriou.net>
2020-05-04 11:01:38 +0200

    Sheet music - Download button has icon now

--- src/components/pdfModal.js
+++ src/components/pdfModal.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react"
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap"
 import slugify from "slugify"
+import { FiDownload } from "react-icons/fi"
+
 import PDFViewer from "./pdfViewer"
 
 export default ({ partition, active, setter, title }) => {
@@ -35,13 +37,18 @@ export default ({ partition, active, setter, title }) => {
 				</ModalBody>
 				<ModalFooter>
 					<Button
+						className="btn-iconed w-75 w-md-50 text-white"
 						color="primary"
 						download={`${slugify(title)}.pdf`}
 						href={partition}
 						title={title}
 					>
-						Download
+						<span className="btn-icon">
+							<FiDownload />
+						</span>
+						<span className="btn-text">Download</span>
 					</Button>
+
 					<Button color="secondary" onClick={toggle}>
 						Close
 					</Button>
