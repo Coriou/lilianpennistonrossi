885555c568b906989438f4ab06edffad12131243
Benjamin C <benjamin@coriou.net>
2020-05-20 12:32:01 +0200

    Home - Added a photo

---
+++ src/components/homePhoto.js
@@ -0,0 +1,33 @@
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+
+export default () => {
+	const {
+		allFile: { edges },
+	} = useStaticQuery(
+		graphql`
+			query {
+				allFile(filter: { name: { eq: "cropped-piano" } }) {
+					edges {
+						node {
+							photo: childImageSharp {
+								fluid(maxWidth: 874, quality: 100) {
+									...GatsbyImageSharpFluid
+								}
+							}
+						}
+					}
+				}
+			}
+		`
+	)
+
+	return (
+		<Img
+			fluid={edges[0].node.photo.fluid}
+			className="home-picture mb-4 mb-md-0"
+			alt="Lilian Penniston-Rossi"
+		/>
+	)
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,14 @@
 import React from "react"
-import { graphql } from "gatsby"
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
+import { graphql, Link } from "gatsby"
+import { Row, Col } from "reactstrap"
+import { Button } from "reactstrap"
+import { FiTv } from "react-icons/fi"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import HomePhoto from "../components/homePhoto"
 
 const IndexPage = ({ data }) => {
 	const {
@@ -17,9 +21,34 @@ const IndexPage = ({ data }) => {
 
 			<article className="mt-4">
 				<MDXProvider className="mt-4">
-					<MDXRenderer>{body}</MDXRenderer>
+					<Row>
+						<Col md={3} lg={2}>
+							<HomePhoto />
+						</Col>
+
+						<Col>
+							<MDXRenderer>{body}</MDXRenderer>
+						</Col>
+					</Row>
 				</MDXProvider>
 			</article>
+
+			<Row>
+				<Col className="d-flex justify-content-center align-content-center mb-6 mt-4">
+					<Button
+						color="primary"
+						className="btn-iconed w-50 w-md-25 text-white"
+						block
+						to={"/v"}
+						tag={Link}
+					>
+						<span className="btn-icon">
+							<FiTv />
+						</span>
+						<span className="btn-text">Videos</span>
+					</Button>
+				</Col>
+			</Row>
 		</Layout>
 	)
 }
