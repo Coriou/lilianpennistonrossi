3187638e2d013aa3f6e2082d7d43078e551604c9
Benjamin Coriou <benjamin@coriou.net>
2020-05-21 15:14:36 +0200

    Disable bundle size analyzer
    Seems like Netlify CMS is fucking it up, can't be bothered to investigate right now

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,6 @@ module.exports = {
 		// 	},
 		// },
 		`gatsby-plugin-mdx`,
-		`gatsby-plugin-webpack-bundle-analyser-v2`,
 		`gatsby-plugin-netlify-cms`,
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
