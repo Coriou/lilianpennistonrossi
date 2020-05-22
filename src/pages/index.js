ba7f705dcd0d4c3d440bb3454b290e4e86313e58
Benjamin Coriou <benjamin@coriou.net>
2020-05-22 17:37:48 +0200

    Link to latest playlist on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import { graphql, Link } from "gatsby"
 import { Row, Col } from "reactstrap"
 import { Button } from "reactstrap"
 import { FiTv } from "react-icons/fi"
+import { BsMusicNoteList } from "react-icons/bs"
 import { MDXProvider } from "@mdx-js/react"
 import { MDXRenderer } from "gatsby-plugin-mdx"
 
@@ -34,10 +35,29 @@ const IndexPage = ({ data }) => {
 			</article>
 
 			<Row>
+				<Col
+					xs={12}
+					sm={6}
+					className="d-flex justify-content-center align-content-center mb-6 mt-4"
+				>
+					<Button
+						color="primary"
+						className="btn-iconed w-100 w-md-50 text-white"
+						block
+						to={"/p/confinement-2020"}
+						tag={Link}
+					>
+						<span className="btn-icon">
+							<BsMusicNoteList />
+						</span>
+						<span className="btn-text">Latest Playlist</span>
+					</Button>
+				</Col>
+
 				<Col className="d-flex justify-content-center align-content-center mb-6 mt-4">
 					<Button
 						color="primary"
-						className="btn-iconed w-50 w-md-25 text-white"
+						className="btn-iconed w-100 w-md-50 text-white"
 						block
 						to={"/v"}
 						tag={Link}
@@ -45,7 +65,7 @@ const IndexPage = ({ data }) => {
 						<span className="btn-icon">
 							<FiTv />
 						</span>
-						<span className="btn-text">Videos</span>
+						<span className="btn-text">All Videos</span>
 					</Button>
 				</Col>
 			</Row>
